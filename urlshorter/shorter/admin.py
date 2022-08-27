5ad867659e409331ca92167e3efc0541a3e49259
Sheikh Sadi Bandan <91241736+Sk-Sadi-Bandan@users.noreply.github.com>
2022-08-27 23:20:04 +0600

    Add files via upload

---
+++ urlshorter/shorter/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from shorter.models import Urls
+
+# Register your models here.
+admin.site.register(Urls)
---
+++ urlshorter/shorter/models.py
@@ -0,0 +1,7 @@
+from pyclbr import Class
+from django.db import models
+
+# Create your models here.
+class Urls(models.Model):
+    link = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=10)
